4af8c84960ba76bb5be305637bcdbf59aa98a23b
Victor Osório <victorporto_@hotmail.com>
2023-04-02 13:04:40 -0300

    Alteracoes de layout

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -2,10 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
-  templateUrl: './titulo.component.html'
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
   @Input() titulo : string | undefined;
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = '';
+  @Input() botaoListar = false;
   constructor() { }
 
   ngOnInit() {
